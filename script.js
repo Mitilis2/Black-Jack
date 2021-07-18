02ea50ac57400754abf7e05eb1816c49fc19c059
Mitul Shah <mitulshah91@gmail.com>
2021-07-18 19:24:18 -0400

    finish styling game and messages

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+// elements used
+
+const countOne = document.getElementById('countOne');
+const countTwo = document.getElementById('countTwo');
+
+
+// starting game conditions
+
+countOne.textContent = 0;
+countTwo.textContent = 0;
