42d2ae702fe8ff7896283d37e7448c2a09dda743
Mitul Shah <mitulshah91@gmail.com>
2021-07-18 20:03:17 -0400

    add instructions, bring in elements to JS

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 'use strict';
 // elements used
+    //player cards
+const playerOneCard = document.getElementById('player-one');
+const playerTwoCard = document.getElementById('player-two');
+const playerCountOne = document.getElementById('countOne');
+const playerCountTwo = document.getElementById('countTwo');
+//buttons
+const hitButton = document.getElementsByClassName('hit-btn');
+const holdButton = document.getElementsByClassName('hold-btn');
+const resetButton = document.getElementsByClassName('reset-btn');
+// deck image
+const deckOfCards = document.getElementById('deck-img');
 
-const countOne = document.getElementById('countOne');
-const countTwo = document.getElementById('countTwo');
+// messages
 
+
+//other
 
 // starting game conditions
+let count, activePlayer, playing;
 
 countOne.textContent = 0;
 countTwo.textContent = 0;
+function changeDeck(){
+    deckOfCards.src='Cards/JPEG/purple_back.jpg'
+};
+
+changeDeck();
