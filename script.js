30e213a5c5289e23b3e3729de9d1a402ca05f902
Mitul Shah <mitulshah91@gmail.com>
2021-07-18 20:17:23 -0400

    style instructions

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 'use strict';
 // elements used
     //player cards
-const playerOneCard = document.getElementById('player-one');
-const playerTwoCard = document.getElementById('player-two');
-const playerCountOne = document.getElementById('countOne');
-const playerCountTwo = document.getElementById('countTwo');
-//buttons
-const hitButton = document.getElementsByClassName('hit-btn');
-const holdButton = document.getElementsByClassName('hold-btn');
-const resetButton = document.getElementsByClassName('reset-btn');
-// deck image
-const deckOfCards = document.getElementById('deck-img');
+        const playerOneCard = document.getElementById('player-one');
+        const playerTwoCard = document.getElementById('player-two');
+        const playerCountOne = document.getElementById('countOne');
+        const playerCountTwo = document.getElementById('countTwo');
+    //buttons
+        const hitButton = document.getElementsByClassName('hit-btn');
+        const holdButton = document.getElementsByClassName('hold-btn');
+        const resetButton = document.getElementsByClassName('reset-btn');
+    // deck image
+        const deckOfCards = document.getElementById('deck-img');
 
-// messages
+    // messages
+        const winnerMessage = document.querySelector('.winner-message');
 
-
-//other
+    //other
 
 // starting game conditions
-let count, activePlayer, playing;
-
-countOne.textContent = 0;
-countTwo.textContent = 0;
-function changeDeck(){
-    deckOfCards.src='Cards/JPEG/purple_back.jpg'
-};
-
-changeDeck();
+    let count, activePlayer, playing;
